6580d15d2f55d030a547a23286d4c041d1d32d89
TonyM10 <anthony.mon@live.com>
2025-05-14 11:19:48 -0600

    Updated main.py to read a file and count words in file

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("greetings boots")
+from stats import word_count
+
+def get_book_text(file_path):
+    with open(file_path) as file:
+        book = file.read()
+    return book
+
+def main():
+    frankenstein = get_book_text("books/frankenstein.txt")
+    frankenstein_word_count = word_count(frankenstein)
+    return print(f"{frankenstein_word_count} words found in the document")
+
+main()
---
+++ stats.py
@@ -0,0 +1,2 @@
+def word_count(text_file):
+    return len(text_file.split())
