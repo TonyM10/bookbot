3f5d92c3757556b2faef955e496c4166bada3045
TonyM10 <anthony.mon@live.com>
2025-05-15 15:24:15 -0600

    started a new function in stats.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import word_count
+from stats import word_count, character_count
 
 def get_book_text(file_path):
     with open(file_path) as file:
@@ -8,6 +8,8 @@ def get_book_text(file_path):
 def main():
     frankenstein = get_book_text("books/frankenstein.txt")
     frankenstein_word_count = word_count(frankenstein)
-    return print(f"{frankenstein_word_count} words found in the document")
+    print(f"{frankenstein_word_count} words found in the document")
+    frankenstein_char_count = character_count(frankenstein)
+    print(frankenstein_char_count)
 
 main()
--- stats.py
+++ stats.py
@@ -1,2 +1,14 @@
 def word_count(text_file):
     return len(text_file.split())
+
+def character_count(text_file):
+    character_counts = {}
+    for char in text_file.lower():
+        if char in character_counts:
+            character_counts[char] += 1
+        else:
+            character_counts[char] = 1
+    return character_counts
+
+def new_funct():
+    return
