4f415edde2b118584ff7025393b4836621571980
TonyM10 <anthony.mon@live.com>
2025-05-16 22:37:14 -0600

    Added sort_character_count function and printed results

--- main.py
+++ main.py
@@ -1,15 +1,43 @@
-from stats import word_count, character_count
+from stats import word_count, character_count, sort_character_count
+import sys
 
 def get_book_text(file_path):
-    with open(file_path) as file:
-        book = file.read()
-    return book
+    try:
+        with open(file_path) as file:
+            book = file.read()
+        return book
+    except FileNotFoundError:
+        print(f"Error: The file '{file_path}' was not found.")
+        sys.exit(1)
+    except Exception as e:
+        print(f"An error occurred while reading the file: {e}")
+        sys.exit(1)
 
 def main():
-    frankenstein = get_book_text("books/frankenstein.txt")
+    book_path = "books/frankenstein.txt"
+    frankenstein = get_book_text(book_path)
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_path}...")
+    print("---------------------------------") 
+
+    print("----------- Word Count ----------")
     frankenstein_word_count = word_count(frankenstein)
-    print(f"{frankenstein_word_count} words found in the document")
+    print(f"Found {frankenstein_word_count} total words")
+    print("---------------------------------")
+
+    print("--------- Character Count -------")
     frankenstein_char_count = character_count(frankenstein)
-    print(frankenstein_char_count)
+    sorted_chars = sort_character_count(frankenstein_char_count)
+
+    for item in sorted_chars:
+        char = item['character']
+        count = item['count']
+        # Check if the character is an alphabetical character
+        if char.isalpha():
+            # Print the character and its count
+            print(f"{char}: {count}")
+
+    print("---------------------------------")
+    print("=========== Analysis Complete ===========")
 
 main()
--- stats.py
+++ stats.py
@@ -1,6 +1,9 @@
+# This function counts the number of words in a text file
 def word_count(text_file):
     return len(text_file.split())
 
+# Counts each character in a text file and creates a dictionary
+# of each character and their count.
 def character_count(text_file):
     character_counts = {}
     for char in text_file.lower():
@@ -10,5 +13,10 @@ def character_count(text_file):
             character_counts[char] = 1
     return character_counts
 
-def new_funct():
-    return
+# Sorts a dictionary of characters and their counts from
+# greatest to least and prints alphabetical characters.
+def sort_character_count(character_dict):
+    items = list(character_dict.items())
+    sort_items = sorted(items, key=lambda item: item[1], reverse=True)
+    sorted_list_dict = [{'character': char, 'count': count} for char, count in sort_items]
+    return sorted_list_dict
